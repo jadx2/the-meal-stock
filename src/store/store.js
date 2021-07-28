dd1824dae149a9f2db3c6222c728770cb24d0ae3
jadx2 <delmarjaim@gmail.com>
2021-07-28 09:27:37 -0500

    Remove developers tool

--- src/store/store.js
+++ src/store/store.js
@@ -1,4 +1,4 @@
-import { createStore, applyMiddleware, compose } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from '../reducers';
 
@@ -10,17 +10,6 @@ const initialState = {
   error: null,
 };
 
-/* eslint-disable no-underscore-dangle */
-/* eslint-disable operator-linebreak */
-const store = createStore(
-  rootReducer,
-  initialState,
-  compose(
-    applyMiddleware(thunk),
-    window.__REDUX_DEVTOOLS_EXTENSION__ &&
-      window.__REDUX_DEVTOOLS_EXTENSION__(),
-  ),
-);
-/* eslint-enable */
+const store = createStore(rootReducer, initialState, applyMiddleware(thunk));
 
 export default store;
